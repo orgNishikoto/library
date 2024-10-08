cd222fb0eaf6c598c2af57ac1cc58940280aed15
Nishi <james.renaud.75@gmail.com>
2024-10-08 17:12:57 +0200

    update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes';
 
 name 'Library';
 author 'Nishikoto <https://www.nishikoto.fr>';
-version 'fork-alpha-0.1.2';
+version 'fork-alpha-0.1.3';
 
 shared_script '@lib/imports.lua';
 shared_script 'imports.lua';
